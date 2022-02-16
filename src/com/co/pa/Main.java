4e92fad87467ba73160ad7496e7f6424184178a9
Luis Eduardo Bolivar Cardenas <theluisbolivar@192.168.1.2>
2022-02-16 15:59:12 -0600

    Reduce some code

--- src/com/co/pa/Main.java
+++ src/com/co/pa/Main.java
@@ -6,32 +6,24 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static int LongestConsecutive(int[] arr) {
-        Arrays.sort(arr);
-        return getMaxCount(arr);
-    }
-
-    public static void main (String[] args) {
-        //6, 7, 3, 1, 100, 102, 6, 12
-        //5, 6, 1, 2, 8, 9, 7
-        int[] arr = new int[]{6, 7, 3, 1, 100, 102, 6, 12};
-        System.out.print(LongestConsecutive(arr));
-    }
-
-    public static int getMaxCount(int[] arr) {
         int max = 0;
-        Set<Integer> control = new HashSet();
+        Arrays.sort(arr);
+        Set<Integer> control = new HashSet<>();
         for(int i = 0; i < (arr.length - 1); i++){
-            int current = arr[i];
-            int next = arr[(i + 1)];
-
-            if((current + 1) == next) {
-                control.add(current);
-                control.add(next);
+            if((arr[i] + 1) == arr[(i + 1)]) {
+                control.add(arr[i]);
+                control.add(arr[(i + 1)]);
             } else {
                 max = control.size() > max ? control.size() : max;
                 control = new HashSet<>();
             }
         }
         return control.size() > max ? control.size() : max;
+    }
+
+    public static void main (String[] args) {
+        //6, 7, 3, 1, 100, 102, 6, 12 //5, 6, 1, 2, 8, 9, 7
+        int[] arr = new int[]{6, 7, 3, 1, 100, 102, 6, 12};
+        System.out.print(LongestConsecutive(arr));
     }
 }
